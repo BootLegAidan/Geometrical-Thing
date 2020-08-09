061f9b2be9013a6caf6a2704e5328e6683420094
BootLegAidan <30330512+BootLegAidan@users.noreply.github.com>
2020-08-09 12:45:25 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -6,17 +6,25 @@ var points = document.getElementById("pointNum");
 var pauseToggleEl = document.getElementById("pauseToggle");
 var colorToggleEl = document.getElementById("colorToggle");
 var nightModeToggleEl = document.getElementById('nightModeToggle')
+var morphToggleEl = document.getElementById('morphToggle')
+var shapeToggleEl = document.getElementById('shapeToggle')
 let root = document.documentElement;
 
 reset.onclick = function () {
 	num = 2;
+	fakeNum = num * 100
+	cosMult = 1
+	sinMult = 1
 };
 
 var ctx = c.getContext("2d");
 
 var num = 2;
-
+var fakeNum = num * 100
 var pause = 0;
+
+var cosMult = 1.0
+var sinMult = 1.0
 
 var size;
 
